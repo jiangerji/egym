9f94e6d7000b30897ca329c11a6121b8f866925c
jiangerji <jiangerji@gmail.com>
2015-03-23 13:43:28 +0800

    add version 2

--- lbs/python/python-geohash-0.8.4/test_genhash.py
+++ lbs/python/python-geohash-0.8.4/test_genhash.py
@@ -100,60 +100,161 @@ import sqlite3
 latitude = 30.178903
 longitude = 45.698545
 
+print latitude, longitude
+
+# tttLa, tttLo = (30.189125, 45.699383)
+# print getDistance((latitude, longitude), (tttLa, tttLo))
+# exit()
+
 def testLBS():
     cx = sqlite3.connect("lbs.db")
-    cx.execute('CREATE  TABLE  IF NOT EXISTS "lbs" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "latitude" FLOAT NOT NULL , "longitude" FLOAT NOT NULL , "geohash" VARCHAR(12) NOT NULL )')
+    cx.execute('CREATE  TABLE  IF NOT EXISTS "lbs" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE , "latitude" FLOAT NOT NULL , "longitude" FLOAT NOT NULL , "geohash" VARCHAR(12) NOT NULL , "geohash2" VARCHAR(12) NOT NULL , "geohash5" VARCHAR(12) NOT NULL , "geohash10" VARCHAR(12) NOT NULL , "distance" INTEGER NOT NULL )')
 
-    insertCmd = 'INSERT INTO "lbs" ("latitude","longitude","geohash") VALUES (?,?,?)'
+    insertCmd = 'INSERT INTO "lbs" ("latitude","longitude","geohash","geohash2","geohash5","geohash10", "distance") VALUES (?,?,?,?,?,?,?)'
 
     # 生成100000个半径100多公里范围位置信息
-    if False:
+    if True:
         for i in xrange(10**5):
             deltaLat = random.randint(-1, 1) + random.randint(0, 350000) * 0.000001
             deltaLon = random.randint(-1, 1) + random.randint(0, 350000) * 0.000001
 
-            realLat = latitude + deltaLat
-            realLon = longitude + deltaLon
-            posGeoHash = geohash.encode(float("%.2f"%realLat), float("%.2f"%realLon))
-            cx.execute(insertCmd, ("%.6f"%realLat, "%.6f"%realLon, posGeoHash))
-    cx.commit()
+            tLat = float("%.6f"%(latitude + deltaLat))
+            tLon = float("%.6f"%(longitude + deltaLon))
 
-    cu.close()
+            # realLat = float("%.2f"%(latitude + deltaLat))
+            # realLon = float("%.2f"%(longitude + deltaLon))
+            realLat = (int(tLat*100)) / 100.0
+            realLon = (int(tLon*100)) / 100.0
+
+            # 0.01表示1公里左右的区域
+            posGeoHash = geohash.encode(realLat, realLon)
+
+            # 0.02表示2公里
+            realLat_2 = (int(realLat*100) / 2 * 2) / 100.0
+            realLon_2 = (int(realLon*100) / 2 * 2) / 100.0
+            posGeoHash_2 = geohash.encode(realLat_2, realLon_2)
+
+            # 0.05表示5公里直径的区域
+            realLat_5 = (int(realLat*100) / 5 * 5) / 100.0
+            realLon_5 = (int(realLon*100) / 5 * 5) / 100.0
+            posGeoHash_5 = geohash.encode(realLat_5, realLon_5)
+
+            # 0.1 表示10公里左右
+            realLat_10 = (int(tLat*10)) / 10.0
+            realLon_10 = (int(tLon*10)) / 10.0
+            posGeoHash_10 = geohash.encode(realLat_10, realLon_10)
+
+            distance = int(1000*getDistance((latitude, longitude), (tLat, tLon)))
+
+            cx.execute(insertCmd, (tLat, tLon, posGeoHash, posGeoHash_2, posGeoHash_5, posGeoHash_10, distance))
+    cx.commit()
     cx.close()
 
 # 获取距离内的位置
 def getNeighbours(levelQuota=1):
+    if levelQuota > 4:
+        levelQuota = 4
+    elif levelQuota < 1:
+        levelQuota = 1
+
     cx = sqlite3.connect("lbs.db")
 
     selectCmd = "select * from lbs where geohash='%s'"
+
+    latlons = []
 
     # 找出最近的item
     cu=cx.cursor()
     orthLatitude = float("%.2f"%(math.floor(latitude*100)/100))
     orthLongitude = float("%.2f"%(math.floor(longitude*100)/100))
-
     latlon = (orthLatitude, orthLongitude)
-    print latlon, geohash.encode(orthLatitude, orthLongitude)
+    latlons.append(latlon)
+
+    orthLatitude_2 = (int(latitude*100) / 2 * 2) / 100.0
+    orthLongitude_2 = (int(longitude*100) / 2 * 2) / 100.0
+    latlon = (orthLatitude_2, orthLongitude_2)
+    latlons.append(latlon)
+
+    orthLatitude_5 = (int(latitude*100) / 5 * 5) / 100.0
+    orthLongitude_5 = (int(longitude*100) / 5 * 5) / 100.0
+    latlon = (orthLatitude_5, orthLongitude_5)
+    latlons.append(latlon)
+    
+    orthLatitude_10 = float("%.1f"%(math.floor(latitude*10)/10))
+    orthLongitude_10 = float("%.1f"%(math.floor(longitude*10)/10))
+    latlon = (orthLatitude_10, orthLongitude_10)
+    latlons.append(latlon)
+
+    print latlons
 
     # levelQuota = level
-    for level in range(1, levelQuota):
-        neighbours = getNeighbourArea(latlon, level)
-        print "Level", level
-        for i in neighbours:
-            lat, lon = geohash.decode(i)
-            cmd = selectCmd%i
-            cu.execute(cmd)
-            datas = cu.fetchall()
-            if len(datas) > 0:
-                for data in datas:
-                    nLat = data[1]
-                    nLon = data[2]
-                    # print i
-                    distance = getDistance((latitude, longitude), (nLat, nLon))
-                    print "  %10d %f %f %6d"%(data[0], nLat, nLon, distance*1000), i
+    for level in range(1, levelQuota+1):
+        latlon = latlons[level-1]
+        print "Level", level, latlon
+        getNeighbourArea(cu, latlon, level)
+        # print latlon, geohash.encode(orthLatitude, orthLongitude)
+
+        # for i in neighbours:
+        #     lat, lon = geohash.decode(i)
+        #     cmd = selectCmd%i
+        #     cu.execute(cmd)
+        #     datas = cu.fetchall()
+        #     if len(datas) > 0:
+        #         for data in datas:
+        #             nLat = data[1]
+        #             nLon = data[2]
+        #             # print i
+        #             distance = getDistance((latitude, longitude), (nLat, nLon))
+        #             print "  %10d %f %f %6d"%(data[0], nLat, nLon, distance*1000), i
 
 
-def getNeighbourArea(latlon, level=1):
+
+def getNeighbourArea_1(cursor, latlon):
+    level = 1
+
+    sLatitude = latlon[0]
+    sLongitude = latlon[1]
+
+    geohashCode = geohash.encode(sLatitude, sLongitude)
+    selectCmdFormat = "select * from lbs where geohash='%s';"
+    
+    # 获取相邻的4个象限
+    orthLatitude = float("%.2f"%(math.floor(latitude*100)/100))
+    orthLongitude = float("%.2f"%(math.floor(longitude*100)/100))
+
+def getNeighbourArea(cursor, latlon, level=1):
+    if level < 1:
+        level = 1
+    elif level > 4:
+        level = 4
+
+    sLatitude = latlon[0]
+    sLongitude = latlon[1]
+
+    geohashCode = geohash.encode(sLatitude, sLongitude)
+
+    selectParams = ["geohash", "geohash2", "geohash5", "geohash10"]
+    selectCmdFormat = "select * from lbs where %s='%s';"
+
+    selectCmd = selectCmdFormat%(selectParams[level-1], geohashCode)
+    print selectCmd
+
+    cursor.execute(selectCmd)
+    datas = cursor.fetchall()
+    if len(datas) > 0:
+        for data in datas:
+            nLat = data[1]
+            nLon = data[2]
+            # print i
+            distance = getDistance((latitude, longitude), (nLat, nLon))
+            print "  %10d %f %f %6d"%(data[0], nLat, nLon, distance*1000)
+
+def getNeighbourArea1(latlon, level=1):
+    if level < 1:
+        level = 1
+    elif level > 4:
+        level = 4
+
     latitude = latlon[0]
     longitude = latlon[1]
 
@@ -182,8 +283,8 @@ def getNeighbourArea(latlon, level=1):
 
     return neighbours
 
-# testLBS()
-getNeighbours(levelQuota=5)
+testLBS()
+# getNeighbours(levelQuota=5)
 
 # python -m profile -s time test_genhash.py
          
